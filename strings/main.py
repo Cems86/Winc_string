02f9283fe76ca3c346c6b60eeb3a9e0f0e8f87a5
Cems86 <125822968+Cems86@users.noreply.github.com>
2023-02-26 18:57:51 +0100

    Add files via upload
    Added changes to the code for first_name and last_name

--- strings/main.py
+++ strings/main.py
@@ -7,14 +7,19 @@ score_0='Ruud Gullit'
 score_1='Marco van Basten'
 goal_0=int(32)
 goal_1=int(54)
+
 scorers=f"{score_0} {goal_0}, {score_1} {goal_1}"
 report=f'{score_0} scored in the {goal_0}nd minute\n{score_1} scored in the {goal_1}th minute'
+
 player='Ronald Koeman'
-first_name=player[:6]
-last_name=player[7:]
+first_name=player[:player.find(" ")]
+last_name= player[player.find(" "):][1:] 
 last_name_len=len(last_name)
 name_short=f'{first_name[0]}. {last_name}'
-chant=(len(first_name)* (f'{first_name}! ')) [:-1]
+
+chant=(len(first_name)* f'{first_name}! ') [:-1]
 good_chant=chant[:-1]!=str(' ')
 chant_l=len(chant)
 print(good_chant)
+print (first_name)
+print (last_name)
